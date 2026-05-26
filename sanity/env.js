0647f7f78c52be03deeed299fe00fb62561d219b
HarshikaJain <76556299+HarshikaJain@users.noreply.github.com>
2026-05-26 16:52:39 +0530

    Update env.js

--- sanity/env.js
+++ sanity/env.js
@@ -1,5 +1,3 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-05-16'
-
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-29'
+export const dataset = 'production'
+export const projectId = 'jcpsiivn' 
