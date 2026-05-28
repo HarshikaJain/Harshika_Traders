f76810f407a61d30f2eebb823852d03402e5bffb
HarshikaJain <76556299+HarshikaJain@users.noreply.github.com>
2026-05-28 19:20:43 +0530

    Update sanity.config.js

--- sanity.config.js
+++ sanity.config.js
@@ -1,12 +1,17 @@
+import { defineConfig } from 'sanity'; // Add this line right at the top
+import { structureTool } from 'sanity/structure';
+import { visionTool } from 'sanity/vision';
+import { schema } from './sanity/schemaTypes'; // Verify your exact path match
+
 export default defineConfig({
   name: 'default',
   title: 'Harshika Traders',
-  projectId: 'jcpsiivn', // Change this back to your true Project ID
+  projectId: 'jcpsiivn', 
   dataset: 'production',
   basePath: '/admin', 
   schema,
   plugins: [
-    structureTool({structure}),
-    visionTool({defaultApiVersion: apiVersion}),
+    structureTool(),
+    visionTool({ defaultApiVersion: '2024-03-11' }),
   ],
-})
+});
