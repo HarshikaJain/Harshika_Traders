651286ba8b9a2575c8e58445df07179239fca625
HarshikaJain <76556299+HarshikaJain@users.noreply.github.com>
2026-06-04 21:58:18 +0530

    Update index.js

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -1,6 +1,6 @@
 import product from './product'; // Your existing untouchable product schema
 import banner from './banner';   // Add this line safely
 
-export const schema = {
+export const schemaTypes = {
   types: [product, banner],
 };
